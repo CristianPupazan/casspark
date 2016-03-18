82d9bb192549cc748190a3bc7539349e8c03ed32
Cristian Pupazan <cristian-ovidiu.pupazan@springer.com>
2016-03-18 11:53:02 +0000

    renamed test

--- spark-count/src/test/scala/org/cristianpupazan/example/count/SimpleSparkCountTest.scala
+++ spark-count/src/test/scala/org/cristianpupazan/example/count/SimpleSparkCountTest.scala
@@ -4,9 +4,9 @@ import org.scalatest.{Matchers, FunSpec}
 
 class SimpleSparkCountTest extends FunSpec with Matchers with TestSparkContext {
 
-  describe("dummy test") {
+  describe("Simple Spark Count") {
 
-    it("should count characters of a given word") {
+    it("should count some numbers") {
       new SimpleSparkCount(sc).count(1 to Int.MaxValue) shouldBe Int.MaxValue
     }
   }
